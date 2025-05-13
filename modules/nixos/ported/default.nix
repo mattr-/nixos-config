f734651d1ed39b0d69afcb346e733d1c065316e7
Matt Rogers <codemattr@gmail.com>
2025-05-12 19:01:02 -0500

    refactor: change up the firefox config
    clear the config and use the firefox-bin package.
    i want my tab groups.

--- modules/nixos/ported/default.nix
+++ modules/nixos/ported/default.nix
@@ -250,30 +250,7 @@ in
 
   programs.firefox = {
     enable = true;
-    package = pkgs.firefox;
-    policies = {
-      DisableTelemetry = true;
-      DisableFirefoxStudies = true;
-      DontCheckDefaultBrowser = true;
-      DisablePocket = true;
-      SearchBar = "unified";
-
-      Preferences = {
-        "extensions.pocket.enabled" = lock-false;
-        "browser.newtabpage.pinned" = lock-empty-string;
-        "browser.topsites.contile.enabled" = lock-false;
-        "browser.newtabpage.activity-stream.showSponsored" = lock-false;
-        "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
-        "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
-      };
-
-      ExtensionSettings = {
-        "uBlock0@raymondhill.net" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-          installation_mode = "force_installed";
-        };
-      };
-    };
+    package = pkgs.firefox-bin;
   };
 
   fonts = {
