aadcf500fc8e92f1a93b8032e5427b1046a3e11a
Matt Rogers <codemattr@gmail.com>
2025-10-12 12:42:21 -0500

    fix: remove amdvlk
    it's been deprecated

--- modules/nixos/ported/default.nix
+++ modules/nixos/ported/default.nix
@@ -105,13 +105,11 @@ in
       libva
       vaapiVdpau
       libvdpau-va-gl
-      amdvlk
       mesa
     ];
     extraPackages32 = with pkgs.pkgsi686Linux; [
       vaapiVdpau
       libvdpau-va-gl
-      amdvlk
     ];
   };
 
