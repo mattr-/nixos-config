0eab90025605c76fd641d44fa7f83fc371926516
Matt Rogers <codemattr@gmail.com>
2025-12-13 09:28:07 -0600

    fix: typo

--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,6 @@
 {
   flake = {
     homeModules = import ./home; # My Custom modules for home-manager
-    nixosModules = import ./nixos; # My Custom modules for home-manager
+    nixosModules = import ./nixos; # My Custom modules for nixos
   };
 }
