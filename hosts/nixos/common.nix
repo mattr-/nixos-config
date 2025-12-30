534e1d743cc68fcceca56e9a40e5a836729024bd
Matt Rogers <codemattr@gmail.com>
2025-12-30 10:41:08 -0600

    refactor: remove vm settings duplication
    no need to duplicate these.

--- hosts/nixos/common.nix
+++ hosts/nixos/common.nix
@@ -1,19 +1,19 @@
+let
+  vmMachineConfig = {
+    memorySize = 8192; # 8GB of RAM
+    cores = 4; # 4 cores
+  };
+in
 {
   # better settings for nixos-rebuild build-vm
-  virtualisation.vmVariant = {
-    virtualisation = {
-      memorySize = 8192; # 8GB of RAM
-        cores = 4; # 4 cores
-    };
-  };
-
-  virtualisation.vmVariantWithBootLoader = {
-    virtualisation = {
-      memorySize = 8192; # 8GB of RAM
-        cores = 4; # 4 cores
-    };
-  };
+  #
+  # the defaults are very low and very minimal and that won't support the
+  # testing i want to do with build-vm.
+  virtualisation.vmVariant.virtualisation = vmMachineConfig;
+  virtualisation.vmVariantWithBootLoader.virtualisation = vmMachineConfig;
 
 
+  # NixOS 24.05 is the first version of NixOS ran with this config.
+  # Do not change this value.
   system.stateVersion = "24.05";
 }
