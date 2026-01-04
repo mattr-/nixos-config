90fca8a8ccac8788247e0d21fe38165551c894cd
Matt Rogers <codemattr@gmail.com>
2026-01-04 12:22:21 -0600

    refactor: switch to explicit module imports

--- hosts/nixos/knid/default.nix
+++ hosts/nixos/knid/default.nix
@@ -1,8 +1,28 @@
 { inputs, pkgs, lib, self, config, ... }:
 {
-  imports =
-    self.lib.directoryImport ./.
-    ++ (builtins.attrValues self.nixosModules);
+  imports = self.lib.directoryImport ./. ++ [
+    self.nixosModules.boot
+    self.nixosModules.nix
+    self.nixosModules.users
+    self.nixosModules.network
+    self.nixosModules.home-manager
+    self.nixosModules.bluetooth
+    self.nixosModules.flatpak
+    self.nixosModules.gnome
+    self.nixosModules.wayland
+    self.nixosModules.graphics
+    self.nixosModules.games
+    self.nixosModules.utils
+    self.nixosModules.ported
+  ];
 
-  hm.imports = builtins.attrValues self.homeModules;
+  hm.imports = with self.homeModules; [
+    cli
+    home
+    go
+    gnome
+    gtk
+    wayland
+    ported
+  ];
 }
