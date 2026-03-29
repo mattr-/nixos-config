b2dd88967fa397a4d1d6e68c243fc5c37d98ad67
Matt Rogers <codemattr@gmail.com>
2026-03-29 17:05:49 -0500

    feat: move off the default sddm theme

--- modules/nixos/sddm/default.nix
+++ modules/nixos/sddm/default.nix
@@ -1,8 +1,17 @@
-{ config, ... }:
+{ config, pkgs, ... }:
+let
+  sddm-theme = pkgs.sddm-astronaut.override {
+    embeddedTheme = "pixel_sakura";
+  };
+in
 {
+  environment.systemPackages = [ sddm-theme ];
+
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
+    theme = "sddm-astronaut-theme";
+    extraPackages = [ sddm-theme ];
     settings.General.GreeterEnvironment = "QT_SCREEN_SCALE_FACTORS=${toString config.dots.display.scale}";
   };
 
