f2530e0444ecf97106277204cbd8029471fee0b7
Matt Rogers <codemattr@gmail.com>
2026-03-29 15:34:24 -0500

    feat: run sddm at 2x scale

--- modules/nixos/sddm/default.nix
+++ modules/nixos/sddm/default.nix
@@ -2,6 +2,7 @@
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
+    settings.General.GreeterEnvironment = "QT_SCREEN_SCALE_FACTORS=2";
   };
 
   nixpkgs.overlays = [
